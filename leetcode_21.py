b1b661d3ff522f3a0076e20d9e23e9bf87e4d50c
Ruhi1010 <rashedial52@gmail.com>
2026-08-23 18:44:54 +0600

    .

---
+++ leetcode_21.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+# Assuming ListNode is defined elsewhere in the codebase
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        cur = dummy = ListNode()
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
+    
+s = Solution()
+# Example usage:
+list1 = ListNode(1, ListNode(2, ListNode(4)))
+list2 = ListNode(1, ListNode(3, ListNode(4)))
+merged_list = s.mergeTwoLists(list1, list2)
+print("Merged list:", end=" ")
+while merged_list:
+    print(merged_list.val, end=" -> ")
+    merged_list = merged_list.next
+print("None")
