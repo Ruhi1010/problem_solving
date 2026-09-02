7f11c9127d13e3dc178352439b4479a63efd6fcd
Ruhi1010 <rashedial52@gmail.com>
2026-09-03 01:11:09 +0600

    .

---
+++ leetcode_202.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if(n==1 or n==7):
+            return True
+        elif(n<10):
+            return False
+        else:
+            sum =0
+            while(n>0):
+                temp = n%10
+                sum += temp*temp
+                n= n//10
+            return self.isHappy(sum) 
+
+s = Solution()
+print(s.isHappy(2))
